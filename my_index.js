ad40f8378969018e0aac63e1ffbc49d857c8325f
lindsaycarbonell <lindsaycarbonell@gmail.com>
2017-02-19 13:00:36 -0500

    pushing up to show dad

--- my_index.js
+++ my_index.js
@@ -8,29 +8,88 @@ var request = require("request");
 
 const BASE = "http://www2.durhamcountync.gov/sheriff/ips/";
 const INDEX = BASE + "default.aspx";
-//don't need PAGE_MAX
+const VALUES = [1,30,0];
 
 
 
 var extractForm = function(body) {
   var $body = $(body);
   var inputs = $body.find("#form1 input");
-  var releasedPd = {}; //options: Last 30 Days, Last 24 Hours, Incarcerated
+  var state = {};
   inputs.each(function() {
-    releasedPd[this.attribs.id] = this.attribs.value.trim();
+    state[this.attribs.id] = this.attribs.value.trim();
   });
-	console.log(releasedPd);
-	return releasedPd;
+	// console.log(state);
+	return state;
 };
 
-// var extractLinks = function(body) {
-// 	var $body = $(body);
-// 	var links = $body.find('').toArray();
-// 	return links.map(l => url.parse(BASE + l.attribs.href, true));
-// };
-
-var extractData = function(id, body){
+var extractLinks = function(body) {
 	var $body = $(body);
-	var data =
-	//NEED SELECTOR HERE FOR WHAT I NEED
+	var links = $body.find('.rsnavy-bold a[href^="http://vinelink.com/"]').toArray();
+  // console.log(links);
+	return links.map(l => url.parse(BASE + l.attribs.href, true));
+};
+
+// var extractData = function(id, body){
+//   console.log(id);
+// 	var $body = $(body);
+// 	var data = { inmate: { id },  }
+// 	//NEED SELECTOR HERE FOR WHAT I NEED
+// });
+
+async.waterfall([  //ask for the initial page
+  next => request(INDEX, next),
+
+  //get the first page and its nonce values
+  function(response, body, next){
+    var state = extractForm(body);
+    next(null, [body], state);
+    // console.log(body);
+
+  },
+
+  function(pages, state, next){
+
+    async.timesSeries(3, function(n, c){
+      var form = {};
+      for (var k in state){
+        form[k] = state[k];
+      }
+      form.__EVENTTARGET = "table[align='center'] [border='0'] [width='98%']";
+      form.__EVENTARGUMENT = '' + VALUES[n];
+      console.log(form);
+
+      var req = request.post(INDEX, { form },
+      function(err, response, body){
+        if (err) return c(err);
+
+        state = extractForm(body);
+        //console.log(state);
+        pages.push(body);
+        // console.log(body);
+        c();
+      });
+      }, err => next(err, pages));
+  },
+
+  //process the HTML
+  // function(pages, next){
+  //   console.log("Requesting inmate details...");
+  //   var links = [];
+  //   var inmates = [];
+  //
+  //
+  // },
+
+
+
+
+
+
+
+
+
+
+], function(err){
+  console.log(err || "All done!");
 });
